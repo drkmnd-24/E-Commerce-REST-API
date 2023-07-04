6cee9f07f5635a6f61f1ebd8eac6d7376410b556
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-07-04 19:12:28 +0800

    added urls and views

--- product/api/urls.py
+++ product/api/urls.py
@@ -5,4 +5,5 @@ from product import views
 
 urlpatterns = [
     path('products/', views.get_products_list, name='products-list'),
+    path('products/<int:pk>/', views.get_product_details, name='products-detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -12,3 +12,14 @@ def get_products_list(request):
     queryset = Product.objects.all()
     serializer = ProductSerializer(queryset, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_product_details(request, pk):
+    try:
+        queryset = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response({'error': 'product does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(queryset)
+    return Response(serializer.data, status=status.HTTP_200_OK)
