2173a915443f92dfce1a34a4385fb2c7677a4268
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2023-07-04 11:26:08 +0800

    create models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='', blank=False)
+    description = models.TextField(default='', blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    stock = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} | {self.price}'
